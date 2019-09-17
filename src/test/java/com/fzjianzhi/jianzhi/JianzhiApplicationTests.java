03b5f4647e781b84197d9f084a594d7be9a835e8
numb-men <le@hengyumo.cn>
2019-09-17 12:43:20 +0800

    fist

---
+++ src/test/java/com/fzjianzhi/jianzhi/JianzhiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fzjianzhi.jianzhi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JianzhiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
